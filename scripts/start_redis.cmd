ed7cd63a7e6d1757f9b2b7ad8c2d0e3bb1fe4792
duyyudus <duyyudus@gmail.com>
2021-06-01 11:50:08 +0700

    Update windows deps folder structure

--- scripts/start_redis.cmd
+++ scripts/start_redis.cmd
@@ -1,3 +1,3 @@
 @echo off
-cd /d dev_dependences/redis-64.3.0.503
+cd /d ../win_dependences/redis-64.3.0.503
 "./redis-server.exe" redis.windows.conf
--- setup.cmd
+++ setup.cmd
@@ -2,4 +2,4 @@ set PY_DIR=C:\Python38
 
 "%PY_DIR%/python" -m venv venv
 "venv/Scripts/pip" install -e .[dev]
-"venv/Scripts/pip" install PythonMagick-0.9.19-cp38-cp38-win_amd64.whl
+"venv/Scripts/pip" install win_dependences/PythonMagick-0.9.19-cp38-cp38-win_amd64.whl
