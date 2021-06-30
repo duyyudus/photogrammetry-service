17eb6483b6cd65fbda5902a2a3aef50d59f377b1
duyyudus <duyyudus@gmail.com>
2021-06-30 19:14:27 +0700

    WIP color correction step

--- src/photogrammetry_service/img_util.py
+++ src/photogrammetry_service/img_util.py
@@ -5,11 +5,24 @@ Image processing utilities
 from os import path
 from pathlib import Path
 
+import colour
 import imageio
 import rawpy
 import skimage
+from colour_checker_detection import detect_colour_checkers_segmentation
+from colour_checker_detection.detection.segmentation import ColourCheckerSwatchesData
+# from OpenImageIO import ImageInput, ImageOutput
 from PIL import Image
 from skimage import data, filters, io
+
+D65 = colour.CCS_ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65']
+REF_COLOUR_CHECKER = colour.CCS_COLOURCHECKERS['ColorChecker24 - After November 2014']
+REF_SWATCHES = colour.XYZ_to_RGB(
+    colour.xyY_to_XYZ(list(REF_COLOUR_CHECKER.data.values())),
+    REF_COLOUR_CHECKER.illuminant,
+    D65,
+    colour.RGB_COLOURSPACES['sRGB'].matrix_XYZ_to_RGB,
+)
 
 
 def dng_to_png(src: Path, tg: Path):
@@ -40,3 +53,77 @@ def blur(src: Path, tg: Path):
 def png_to_tif(src: Path, tg: Path):
     im = Image.open(src.as_posix())
     im.save(tg.as_posix())
+
+
+def verify_color_swatches(swatches):
+    deviation = []
+    rgb_RCCL = colour.XYZ_to_RGB(
+        colour.xyY_to_XYZ(list(REF_COLOUR_CHECKER.data.values())),
+        D65,
+        D65,
+        colour.RGB_COLOURSPACES['sRGB'].matrix_XYZ_to_RGB,
+    )
+    for swatch in swatches:
+        swatch_cc = colour.colour_correction(swatch, swatch, REF_SWATCHES)
+        CCL = swatch_cc
+        totalsum = 0.0
+        for i in range(len(rgb_RCCL)):
+            totalsum += (
+                abs(rgb_RCCL[i][0] - CCL[i][0])
+                + abs(rgb_RCCL[i][1] - CCL[i][1])
+                + abs(rgb_RCCL[i][2] - CCL[i][2])
+            )
+        deviation.append(totalsum)
+    print("swatches deviations: " + str(deviation))
+    min_d = 100.0
+    min_i = -1
+    for i in range(len(deviation)):
+        if deviation[i] < min_d:
+            min_d = deviation[i]
+            min_i = i
+    print("The lowest devi is: " + str(min_d))
+    result = False
+    if min_d < 3.0:
+        print("devi is near good ! ")
+        result = True
+    elif min_d < 5.0:
+        print("devi is near average ! ")
+        result = True
+    elif min_d < 8.0:
+        print("devi is near BAD ! the COLORS will be off!!!!")
+        result = True
+    else:
+        print("devi is BAD ! the Swatches are completely off!!!!")
+        result = False
+    return result, swatches[min_i]
+
+
+def compute_swatch(color_checker: Path) -> ColourCheckerSwatchesData:
+    """
+    Args:
+        color_checker (Path): blurry TIFF color checker
+    """
+    color_checker_img = colour.cctf_decoding(colour.io.read_image(color_checker.as_posix()))
+    swatches = detect_colour_checkers_segmentation(color_checker_img)
+    vresult, swatch = verify_color_swatches(swatches)
+    return swatch
+
+
+def color_correct(src: Path, tg: Path, swatch: ColourCheckerSwatchesData):
+    """
+    Args:
+        src (Path): DNG image
+        tg (Path): JPG image
+    """
+
+    tmp_tif = src.parent.joinpath(f'{tg.stem}_tmp.tiff')
+    dng_to_tif(src, tmp_tif)
+
+    image = colour.cctf_decoding(colour.io.read_image(tmp_tif.as_posix()))
+    cc_image = colour.colour_correction(image, swatch, REF_SWATCHES, 'Finlayson 2015')
+    cc_tmp_tif = src.parent.joinpath(f'{tg.stem}_cc.tiff')
+    colour.io.write_image(colour.cctf_encoding(cc_image), cc_tmp_tif.as_posix(), bit_depth='uint8')
+
+    final_cc_jpg = src.parent.joinpath(f'{tg.stem}.jpg')
+    img = Image.open(cc_tmp_tif.as_posix())
+    img.save(final_cc_jpg.as_posix(), quality=100, subsampling=0)
--- tmp.py
+++ tmp.py
@@ -1,23 +1,63 @@
-import dramatiq
-import requests
-from dramatiq.brokers.redis import RedisBroker
+import rawpy
+import imageio
 
-redis_broker = RedisBroker(host="localhost", port=6379)
-dramatiq.set_broker(redis_broker)
+import skimage
+from skimage import data, io, filters
+from pathlib import Path
 
-
-@dramatiq.actor
-def count_words(url):
-    response = requests.get(url)
-    count = len(response.text.split(" "))
-    print(f"There are {count} words at {url!r}.")
+from PIL import Image, ImageFilter
+from photogrammetry_service import ext_tool_adaptor, img_util
 
 
-if __name__ == '__main__':
+def dng_to_png():
+    p = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669.dng'
+    p2 = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669.png'
+    with rawpy.imread(p) as raw:
+        rgb = raw.postprocess(
+            highlight_mode=0, no_auto_bright=False, use_camera_wb=True, gamma=(2.4, 12.92)
+        )
+    imageio.imsave(p2, rgb)
 
-    # Synchronously count the words on example.com in the current process
-    # count_words("https://google.com")
 
-    # or send the actor a message so that it may perform the count
-    # later, in a separate process.
-    count_words.send("https://facebook.com")
+def blur():
+    p = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669.png'
+    # p = '/Users/duyyudus/Git/photogrammetry-service/img.png'
+    p2 = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669_blur.png'
+    # p2 = '/Users/duyyudus/Git/photogrammetry-service/blur.png'
+
+    image = skimage.io.imread(fname=p, plugin='pil')
+    blurred = skimage.filters.gaussian(image, sigma=(50, 50), truncate=3.5, multichannel=True)
+    # viewer = ImageViewer(blurred)
+    # viewer.show()
+    skimage.io.imsave(p2, blurred)
+
+
+def png_to_tif():
+    p = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669_blur.png'
+    p2 = '/Users/duyyudus/Git/photogrammetry-service/sample_data/task4/2_DNG/DSC05669_blur.tif'
+    im = Image.open(p)
+    im.save(p2)
+
+
+# dng_to_png()
+# blur()
+# png_to_tif()
+
+# ext_tool_adaptor.run_dng_conversion(
+#     Path('/Users/duyyudus/Git/photogrammetry-service/sample_data/task5/cache/color_checker.ARW'),
+#     Path('/Users/duyyudus/Git/photogrammetry-service/sample_data/task5/cache'),
+#     Path('/Applications/Adobe DNG Converter.app/Contents/MacOS/Adobe DNG Converter'),
+# )
+
+swatch = img_util.compute_swatch(
+    Path(
+        '/Users/duyyudus/Git/photogrammetry-service/sample_data/task5/cache/color_checker_blur.tiff'
+    )
+)
+img_util.color_correct(
+    Path('/Users/duyyudus/Git/photogrammetry-service/sample_data/task5/2_DNG/DSC05670.dng'),
+    Path(
+        '/Users/duyyudus/Git/photogrammetry-service/sample_data/task5/3_COLOR_CORRECTED/DSC05670.jpg'
+    ),
+    swatch,
+)
